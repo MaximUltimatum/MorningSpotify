99d15e4beb963b007221b0eedcd9586a824d94a8
MaximUltimatum <mcder018@live.com>
2019-05-11 01:49:01 -0500

    Better fade in. Play with delay times to your liking

--- Ascendancy.sh
+++ Ascendancy.sh
@@ -5,7 +5,7 @@
 amixer sset Master unmute
 amixer sset Headphone unmute
 amixer sset Speaker+LO unmute
-amixer -D pulse sset Master 20%
+amixer -D pulse sset Master 10%
 
 #this opens spotify to song "Ascendancy." Change the '' to a spotify uri under "share" part for a song. & makes it run in background so we can cancel out to manipulate volume
 spotify --uri='spotify:track:7d1OzUw3hGemiPfBjMR9uH#0:0.01'&
@@ -15,9 +15,9 @@ sleep 4
 "^C"
 
 #This slowly fades in song for gentle wake up. Comment out for a Morning Jolt
-for i in 1 2 3 4 5
+for i in {1..9}
 do
-	sleep 1
-	amixer -D pulse sset Master 10%+
+	sleep 2
+	amixer -D pulse sset Master 5%+
 done
 
